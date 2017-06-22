2ac13c030e7d586e7b3390a10dd8ac24bcbc82fe
katunariccorrea <vinikatunaric@gmail.com>
2017-06-21 21:56:02 -0300

    inserido laço de repetição e estrutura de controle sem prints

--- src/main/java/helloWord.java
+++ src/main/java/helloWord.java
@@ -12,15 +12,12 @@ public class helloWord {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!!!");
-		System.out.println("Hello World!!!");
-		System.out.println("Hello World!!!");
-		System.out.println("Hello World4444!!!");
+
 		System.out.println("Hello World!!!");
 		int num = 10;
 		for (int i = 0; i< num; i++){
 			System.out.print(i+1);
-				if ((i+1) == (num/2){
+				if ((i+1) == (num/2)){
 					System.out.print(" - Metade!!!");
 				}
 				System.out.print("\n");
