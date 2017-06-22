21c25e9fab3627efd231a43ff4203694b7e61968
katunariccorrea <vinikatunaric@gmail.com>
2017-06-21 21:50:10 -0300

    inserido laço de repetição e estrutura de controle

--- src/main/java/helloWord.java
+++ src/main/java/helloWord.java
@@ -17,7 +17,16 @@ public class helloWord {
 		System.out.println("Hello World!!!");
 		System.out.println("Hello World4444!!!");
 		System.out.println("Hello World!!!");
-		// segundo commit
+		int num = 10;
+		for (int i = 0; i< num; i++){
+			System.out.print(i+1);
+				if ((i+1) == (num/2)){
+					System.out.print(" - Metade!!!");
+				}
+				System.out.print("\n");
+		}
+		
+
 	}
 
 }
